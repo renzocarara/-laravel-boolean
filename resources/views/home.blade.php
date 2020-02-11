4fbd0ae88c2a002c2f00039cf336f80a4807c84e
Renzo Carara <renzo.carara@libero.it>
2020-02-11 17:52:49 +0100

    prima sezione(iumbo) in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,29 @@
 
         <section class="jumbo first_section">
 
+            <div class="container d-flex">
+
+                <div class="jumbo-left">
+
+                    <h1>DIVENTA
+                        <strong>SVILUPPATORE WEB</strong></h1>
+                    <div class="jumbo-left-img-wrapper">
+                        <img id="jumbo-left-img" class="img-fluid" alt="Impara da casa tua" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif">
+                    </div>
+                    <p><strong>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</strong></p>
+                    <ul>
+                        <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                        <li><strong>Nessuna competenza</strong> di programmazione richiesta</li>
+                        <li>Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
+                    </ul>
+
+                </div>
+
+                <div class="jumbo-right align-self-center">
+                    <img id="jumbo-right-img" alt="Impara da casa tua" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif">
+                </div>
+
+            </div>
         </section>
 
         <section class="section-1">
