fb38ac38522c0760e594b9d7b9616acf3f480a93
Renzo Carara <renzo.carara@libero.it>
2020-02-11 18:52:26 +0100

    terminato jumbo e prima sezione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,48 +6,62 @@
 @section('content')
 
 <main>
-    <div class="container">
 
-        <section class="jumbo first_section">
+    <section class="jumbo first_section container d-flex">
 
-            <div class="container d-flex">
+        <div class="jumbo-left">
 
-                <div class="jumbo-left">
-
-                    <h1>DIVENTA
-                        <strong>SVILUPPATORE WEB</strong></h1>
-                    <div class="jumbo-left-img-wrapper">
-                        <img id="jumbo-left-img" class="img-fluid" alt="Impara da casa tua" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif">
-                    </div>
-                    <p><strong>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</strong></p>
-                    <ul>
-                        <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
-                        <li><strong>Nessuna competenza</strong> di programmazione richiesta</li>
-                        <li>Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
-                    </ul>
-
-                </div>
-
-                <div class="jumbo-right align-self-center">
-                    <img id="jumbo-right-img" alt="Impara da casa tua" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif">
-                </div>
-
+            <h1>DIVENTA
+                <strong>SVILUPPATORE WEB</strong></h1>
+            <div class="jumbo-left-img-wrapper">
+                <img id="jumbo-left-img" class="img-fluid" alt="Impara da casa tua" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif">
             </div>
-        </section>
+            <p><strong>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</strong></p>
+            <ul>
+                <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                <li><strong>Nessuna competenza</strong> di programmazione richiesta</li>
+                <li>Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
+            </ul>
 
-        <section class="section-1">
+        </div>
 
-        </section>
+        <div class="jumbo-right align-self-center">
+            <img id="jumbo-right-img" alt="Impara da casa tua" src="https://www.boolean.careers/images/homepage/pc-black-gif.gif">
+        </div>
 
-        <section class="section-2">
+    </section>
 
-        </section>
+    <section class="container-fluid section-1">
 
-        <section class="section-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 text-center">
+                    <p>98%</p>
+                    <p>La percentuale dei nostri studenti che ora lavora come web developer, oltre la metà a tempo indeterminato.</p>
+                </div>
 
-        </section>
+                <div class="col-lg-4 text-center">
+                    <p>€ 23.000</p>
+                    <p>Lo stipendio medio lordo di partenza degli studenti assunti dalle nostre aziende partner.</p>
+                </div>
 
-    </div>
+                <div class="col-lg-4 text-center">
+                    <p>#1</p>
+                    <p>Siamo il primo istituto online in Italia per indice di gradimento e risultati conseguiti.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="section-2">
+
+    </section>
+
+    <section class="section-3">
+
+    </section>
+
 </main>
 
 @endsection
