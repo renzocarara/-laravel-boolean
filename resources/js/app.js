7a0be70f26b5e52999c99c65b518ad2fc471c168
Renzo Carara <renzo.carara@libero.it>
2020-02-12 17:57:09 +0100

    sistemata gestione icone '+' e '-' sull'accordion, con js

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,3 +13,39 @@ $(function() {
 $(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+
+$(document).ready(function() {
+
+    // accordion delle FAQ
+    // catturo click sulla domanda
+    $('.question_text').click(function() {
+        togglePlusMinusIcon($(this)); //invoco funzione per fare un toggle tra '+' e '-'
+    });
+
+});
+
+// ---------------------------- FUNCTIONs --------------------------------------
+function togglePlusMinusIcon(that) {
+
+    // mi ricavo il riferimento all'icona associata a quella domanda
+    var iconReference = $(that).children();
+
+    // ricavo il riferimento a quale accordion (left o right) si riferisce la domanda cliccata
+    var whichAccordion = $(that).closest('.accordion').attr('id');
+    console.log("whichAccordion:", whichAccordion);
+
+    // verifco che icona è visualizzata al momento del click
+    if ($(iconReference).hasClass('fa-plus')) {
+
+        // ripristino il '+' su una eventuale altra domanda aperta
+        $('#' + whichAccordion + ' .question_text i').removeClass('fa-minus').addClass('fa-plus');
+
+
+        // cambio icona da + a - sulla domanda cliccata
+        $(iconReference).addClass('fa-minus').removeClass('fa-plus');
+    } else {
+        // cambio icona da - a +
+        $(iconReference).removeClass('fa-minus').addClass('fa-plus');
+    }
+}
